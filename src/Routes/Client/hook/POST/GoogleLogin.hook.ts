52799024f321c40fa099d2d27fc639d52e4fbeda
Raheel1258 <rahilahmad1258@gmail.com>
2025-05-27 23:23:54 +0500

    add google access token

--- src/Routes/Client/hook/POST/GoogleLogin.hook.ts
+++ src/Routes/Client/hook/POST/GoogleLogin.hook.ts
@@ -1,11 +1,14 @@
 import { useMutation } from '@tanstack/react-query';
 import { toast } from 'sonner';
 import { GoogleLoginIn } from '@/Routes/Client/Api/api.routes';
+import EmployeeAuthStore from '@/store/Auth/auth.store';
 
 export default function useGoogleLoginHook() {
+  const { setAccessToken } = EmployeeAuthStore();
   return useMutation({
     mutationFn: (code: string) => GoogleLoginIn(code),
-    onSuccess: async () => {
+    onSuccess: async (response) => {
+      setAccessToken(response.data.access_token);
       toast.success('Login Successful', {
         position: 'bottom-right',
       });
