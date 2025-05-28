fa2939637dc986b4f807ecd2bee9d140ee3b798b
Raheel1258 <rahilahmad1258@gmail.com>
2025-05-28 20:31:35 +0500

    handle local storage on client side

--- src/Utils/Providers/auth.ts
+++ src/Utils/Providers/auth.ts
@@ -2,42 +2,52 @@ import EmployeeAuthStore from '@/store/Auth/auth.store';
 import { signOut } from 'next-auth/react';
 
 export const setAuthToken = (token: string, role: string) => {
-  localStorage.setItem('accessToken', token);
-  document.cookie = `accessToken=${token}; path=/; max-age=${60 * 60 * 24}; secure; samesite=strict`;
-  document.cookie = `userRole=${role}; path=/; max-age=${60 * 60 * 24}; secure; samesite=strict`;
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('accessToken', token);
+    document.cookie = `accessToken=${token}; path=/; max-age=${60 * 60 * 24}; secure; samesite=strict`;
+    document.cookie = `userRole=${role}; path=/; max-age=${60 * 60 * 24}; secure; samesite=strict`;
+  }
   EmployeeAuthStore.getState().setAccessToken(token);
   EmployeeAuthStore.getState().setUserRole(role);
 };
 
 export const clearAuthToken = () => {
-  localStorage.removeItem('accessToken');
-  document.cookie = 'accessToken=; path=/; max-age=0';
-  document.cookie = 'userRole=; path=/; max-age=0';
+  if (typeof window !== 'undefined') {
+    localStorage.removeItem('accessToken');
+    document.cookie = 'accessToken=; path=/; max-age=0';
+    document.cookie = 'userRole=; path=/; max-age=0';
+  }
   EmployeeAuthStore.getState().clearAccessToken();
   EmployeeAuthStore.getState().clearUserRole();
   signOut();
 };
 
-export const getAuthToken = () =>
-  localStorage.getItem('accessToken') ||
-  EmployeeAuthStore.getState().accessToken ||
-  getAuthCookie();
+export const getAuthToken = () => {
+  if (typeof window !== 'undefined') {
+    return (
+      localStorage.getItem('accessToken') ||
+      EmployeeAuthStore.getState().accessToken ||
+      getAuthCookie()
+    );
+  }
+  return EmployeeAuthStore.getState().accessToken;
+};
 
 export const getAuthRole = () => {
-  const role =
-    localStorage.getItem('userRole') ||
-    EmployeeAuthStore.getState().userRole ||
-    getAuthCookie();
-  return role;
+  if (typeof window !== 'undefined') {
+    return (
+      localStorage.getItem('userRole') ||
+      EmployeeAuthStore.getState().userRole ||
+      getAuthCookie()
+    );
+  }
+  return EmployeeAuthStore.getState().userRole;
 };
 
 export const getAuthCookie = () => {
-  const match = document.cookie.match(/(?:^|;\s*)accessToken=([^;]*)/);
-  return match?.[1] ?? '';
-};
-
-export const getAuthHeader = () => {
-  const token = getAuthToken() || getAuthCookie();
-  const role = getAuthRole();
-  return token ? { Authorization: `Bearer ${token}`, role } : { role };
+  if (typeof document !== 'undefined') {
+    const match = document.cookie.match(/(?:^|;\s*)accessToken=([^;]*)/);
+    return match?.[1] ?? '';
+  }
+  return '';
 };
