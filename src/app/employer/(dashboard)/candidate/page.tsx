9524331edd87014d77456632a79d1b45b285daaf
Raheel1258 <rahilahmad1258@gmail.com>
2025-05-29 21:59:11 +0500

    add state variable

--- src/app/employer/(dashboard)/candidate/page.tsx
+++ src/app/employer/(dashboard)/candidate/page.tsx
@@ -22,10 +22,12 @@ import AnalyzeInterviewHook from '@/Routes/Employer/hooks/POST/AnalyzeInterview.
 import { toast } from 'sonner';
 
 export default function CandidatePage() {
-
+  const [currentPage, setCurrentPage] = useState(1);
   const { data: candidatestats, refetch: refetchInterviews } =
     UseDashboardCandidateCardStats();
-   const { data: CandidateTableData, isLoading: tableLoading } = UseGetAllInterview({ page: currentPage });
+  const { data: CandidateTableData, isLoading: tableLoading } = UseGetAllInterview({
+    page: currentPage,
+  });
   const Interviewmutation = AnalyzeInterviewHook();
 
   const [isDialogOpen, setIsDialogOpen] = useState(false);
