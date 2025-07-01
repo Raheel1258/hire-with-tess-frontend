b214d256e8f8dc61cf43a3d040091b1add8f5b12
Sayyam Butt <sayyambutt317@gmail.com>
2025-07-01 15:22:15 +0500

    update google redirect path

--- src/app/interview/component/googleloginbtn.tsx
+++ src/app/interview/component/googleloginbtn.tsx
@@ -8,7 +8,7 @@ interface GoogleLoginButtonProps {
 export function GoogleLoginButton({ redirectTo = '/' }: GoogleLoginButtonProps) {
   const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!;
   const REDIRECT_URI =
-    process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI || 'http://localhost:3000/auth/callback';
+    process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI!;
 
   const googleAuthUrl =
     `https://accounts.google.com/o/oauth2/v2/auth?` +
@@ -26,8 +26,6 @@ export function GoogleLoginButton({ redirectTo = '/' }: GoogleLoginButtonProps) 
         window.location.href = googleAuthUrl;
       }}
       className='sm:w-3xs h-11 mt-4 cursor-pointer p-2 hover:bg-gray-100 hover:text-black'
-      // className="w-max-2xl sm:w-[528px] h-[64px] border-r-[14px] rounded-[14px] border-[1px] mt-4 mb-4 font-[roboto] font-normal bg-transparent text-black
-      //         hover:bg-transparent border-gray-400 flex items-center justify-center gap-2"
     >
       <Image src="/images/google.png" alt="Google Icon" width={20} height={20} />
       Continue with Google
